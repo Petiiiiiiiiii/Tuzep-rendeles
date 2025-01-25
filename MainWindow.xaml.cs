0030d5f27116c97865d30683fbae64655b1f20ff
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2025-01-25 13:45:40 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Unalom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static List<Resource> resources;
+        static Dictionary<string, int> basket;
+        Resource selected;
+        public MainWindow()
+        {
+            InitializeComponent();
+            resources = new();
+            basket = new();
+            FileRead();
+            resourceLBX.ItemsSource = resources.Select(x => x.material);
+        }
+        static void FileRead() 
+        {
+            try
+            {
+                using StreamReader sr = new(@"..\..\..\src\resources.txt");
+                while (!sr.EndOfStream) resources.Add(new Resource(sr.ReadLine()));
+            }
+            catch
+            {
+                MessageBox.Show("Valami hiba történt!","Hiba",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void resourceLBX_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selected = resources.Where(x => x.material == resourceLBX.SelectedItem).First();
+            pickedResourceLBL.Content = selected;
+        }
+
+        private void toBasketBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int unit;
+            if (resourceLBX.SelectedItem == null)
+            {
+                MessageBox.Show("Válassz ki egy anyagot!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else 
+            {
+                if (int.TryParse(unitTXB.Text.Trim(), out unit))
+                {
+                    if (basket.ContainsKey(selected.material))
+                    {
+                        basket[selected.material] += unit;
+                    }
+                    else
+                    {
+                        basket.Add($"{selected.material}", unit);
+                    }
+
+                    basketLBX.Items.Add($"{unit}{selected.unitOfMeasure} {selected.material}");
+                }
+                else MessageBox.Show("Számot írj be!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void orderBTN_Click(object sender, RoutedEventArgs e)
+        {
+            int allPrice = 0;
+
+            foreach (var item in basket) 
+            {
+                Resource temp = resources.Where(x => x.material == item.Key).First();
+                allPrice += item.Value * temp.price;
+            }
+
+            MessageBox.Show($"Sikeres rendelés leadás!\nVégösszeg: {allPrice} Ft.","Siker!",MessageBoxButton.OK,MessageBoxImage.Information);
+
+            using StreamWriter sw = new(@"..\..\..\src\orders.txt",true);
+
+            sw.WriteLine("------------------------");
+
+            foreach (var item in basket)
+            {
+                string materialUnitType = resources.Where(x => x.material == item.Key).First().unitOfMeasure;
+                sw.WriteLine($"{item.Key} - {item.Value} {materialUnitType}");
+            }
+
+            sw.WriteLine($"\n{allPrice} Ft");
+
+            basketLBX.Items.Clear();
+        }
+    }
+}
---
+++ Resource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unalom
+{
+    internal class Resource
+    {
+        public string material { get; set; }
+        public string unitOfMeasure { get; set; }
+        public int price { get; set; }
+
+        public Resource(string line)
+        {
+            var temporary = line.Split(';');
+            this.material = temporary[0];
+            this.unitOfMeasure = temporary[1];
+            this.price = Convert.ToInt32(temporary[2]);
+        }
+        public override string ToString()
+        {
+            return $"{this.material} ({this.unitOfMeasure})";
+        }
+    }
+}
